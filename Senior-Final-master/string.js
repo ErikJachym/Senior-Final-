15587458040bbcb65cca65a64a68a2bc3192f475
ErikJachym <7ejachym@stpatrick.org>
2017-05-04 14:01:08 -0500

    Add files via upload

--- Senior-Final-master/string.js
+++ Senior-Final-master/string.js
@@ -38,21 +38,59 @@ exports.checkLength = function(str){
 
 exports.containsUpper =function(str){
 let hasUpper = false;
+try{
+    for(var i=0; i<str.length; i++){
+        hasUpper= inRange(str[i],65,90);
+        if (hasUpper == true) {
+            break;
+        }
+    }
+}
 
 
 }
 
 
 exports.containsLower =function(str){
-
+    let hasLower = false;
+    try{
+        for(var i=0; i<str.length; i++){
+            hasLower= inRange(str[i],97,122);
+            if (hasLower == true) {
+                break;
+            }
+        }
+    }
 }
 
 
 exports.containsNumerical =function(str){
-
+    let hasNumber = false;
+    try{
+        for(var i=0; i<str.length; i++){
+            hasNumber= inRange(str[i],48,57);
+            if (hasNumber == true) {
+                break;
+            }
+        }
+    }
 }
 
 
 exports.containsSpecial =function(str){
+    let hasSpecial = false;
+    let special= [33,35,36,37,38,42,64,94]
+    try{
+        for(var i=0; i<str.length; i++){
+            for(var x=0;x<special.length; x++){
 
+
+            hasSpecial= inRange(str[i],special[x],special[x]);
+            if (hasSpecial == true) {
+                break;
+            }
+        } if (hasSpecial == true) {
+            break;
+        }}
+    }
 }
